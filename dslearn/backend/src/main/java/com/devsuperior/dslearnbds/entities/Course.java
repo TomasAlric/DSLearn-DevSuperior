8f20a055ea8c1d1d793367b70dc0419445a5b85d
TomasAlric <tomas.alricc@gmail.com>
2022-03-22 14:32:34 -0300

    Criando entidade Offer e realizando os mapeamentos

--- dslearn/backend/src/main/java/com/devsuperior/dslearnbds/entities/Course.java
+++ dslearn/backend/src/main/java/com/devsuperior/dslearnbds/entities/Course.java
@@ -2,6 +2,8 @@ package com.devsuperior.dslearnbds.entities;
 
 import javax.persistence.*;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Entity
@@ -19,7 +21,10 @@ public class Course implements Serializable {
 
     private String imgGrayUri;
 
-    public Course(){
+    @OneToMany(mappedBy = "course")
+    private List<Offer> offers = new ArrayList<>();
+
+    public Course() {
 
     }
 
@@ -60,6 +65,10 @@ public class Course implements Serializable {
 
     public void setImgGrayUri(String imgGrayUri) {
         this.imgGrayUri = imgGrayUri;
+    }
+
+    public List<Offer> getOffers() {
+        return offers;
     }
 
     @Override
