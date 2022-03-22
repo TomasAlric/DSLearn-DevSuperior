144c868dd659787933c7c2456e301b619f5bac65
TomasAlric <tomas.alricc@gmail.com>
2022-03-22 15:54:39 -0300

    Criando entidade e enum Resource, realizando os mapeamentos e inserção no banco

--- dslearn/backend/src/main/java/com/devsuperior/dslearnbds/entities/Offer.java
+++ dslearn/backend/src/main/java/com/devsuperior/dslearnbds/entities/Offer.java
@@ -3,6 +3,8 @@ package com.devsuperior.dslearnbds.entities;
 import javax.persistence.*;
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Entity
@@ -24,7 +26,10 @@ public class Offer implements Serializable {
     @JoinColumn(name = "course_id")
     private Course course;
 
-    public Offer(){
+    @OneToMany(mappedBy = "offer")
+    private List<Resource> resources = new ArrayList<>();
+
+    public Offer() {
 
     }
 
@@ -74,6 +79,10 @@ public class Offer implements Serializable {
 
     public void setCourse(Course course) {
         this.course = course;
+    }
+
+    public List<Resource> getResources() {
+        return resources;
     }
 
     @Override
